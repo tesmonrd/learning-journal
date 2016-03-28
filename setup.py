275646c0adcc75597843aab0011636024f268de8
Rick Tesmond <tesmonrd@gmail.com>
2016-03-27 17:31:23 -0700

    initialize api connection

--- setup.py
+++ setup.py
@@ -54,5 +54,6 @@ setup(name='learning-journal',
       main = learning_journal:main
       [console_scripts]
       initialize_db = learning_journal.scripts.initializedb:main
+      load_api = learning_journal.scripts.apiload:main
       """,
       )
